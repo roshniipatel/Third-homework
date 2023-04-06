c5982023894e6ef99a42508cff2a2d72c206a3ed
Roshni Patel <roshnipatel@Roshnis-MacBook-Pro.local>
2023-04-06 18:18:49 -0500

    added all variables and some functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,53 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+
+// Need the following variables:
+
+    // use .split('') to create arrays?
+    // to hold the password we're building
+    // possibly the mega-array
+
+  // prompt for password length --> stored in a variable
+  // Validate the user input --> if user chooses <8 or >128, alert that they need to choose a valid password length, then they're going to need to restart OR call the function that prompts for length again
+  // prompt --> Do they want uppercase letters?
+  // prompt --> Do they want lowercase letters?
+  // prompt --> Do they want numbers?
+  // prompt --> Do they want special characters?
+  // Validate that the user has chosen at least one character set --> if not, either have them start over OR recursively call the function that prompts for the character sets
+  // With each of those prompts, you need an array of those character types
+    // There needs to be at least one of each selected character type included in the password
+    // Randomly generate a character from each character set as it's chosen and add it to the end of the password that you're building (.push() for an array or .concat() for a string)
+    // for loop:
+      // for (var i = password.length - 1; i < passwordLength; i++)
+    // EITHER keep the character sets separate OR combine them to a mega-array to pull the rest of the characters from
+      // IF keeping the character sets separate, randomly generate a number to tell which array you're pulling from, then randomly generate a number to pull the character from the array (character will by array[number])
+      // IF pushing to a mega-array randomly generate a number to pull a character from the array
+      // IF pushing to a mega-array, remember to set the array back to an empty array at the start of the process!
+  // return the generated password
 
 // Write password to the #password input
+
+    // selected length of the password
+    // array of lowercase letters
+    // array of uppercase letters
+    // array of numbers
+    // array of special characters
+
+var passwordLength = 8;
+var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+var specialCase = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '[', ']', '{','}', '<', '>']; 
+
+function askPrompts() {
+  passwordLength = parseInt(prompt("How many characters would you like your password to have? (10-128 characters"));
+}
+
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -12,3 +58,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// console.log(Math.floor(Math.random() * 26)) ;
